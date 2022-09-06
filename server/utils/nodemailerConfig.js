683c8dff6ad93e3e426c1fed6204346877ebb620
Fc0S4b <fcoj.sabugo@gmail.com>
2022-09-06 17:39:00 -0400

    Send Reset Password Email

--- server/utils/nodemailerConfig.js
+++ server/utils/nodemailerConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   host: 'smtp.ethereal.email',
   port: 587,
   auth: {
-    user: 'dixie90@ethereal.email',
-    pass: 'mTbcM6wdBY7C2fGxxJ',
+    user: 'stewart38@ethereal.email',
+    pass: '9eeyyHKHghApan45G1',
   },
 };
